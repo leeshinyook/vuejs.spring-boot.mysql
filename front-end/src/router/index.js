09bf7e1fd1241a71e080a78eebeab13a24f5bb77
leeshinyook <tlsdbr97@gmail.com>
2020-02-03 21:38:42 +0900

    Add : UI of register page

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import LoginPage from '@/views/LoginPage'
+import RegisterPage from '@/views/RegisterPage'
 Vue.use(Router)
 export default new Router({
   mode: 'history',
@@ -9,6 +10,11 @@ export default new Router({
     path: '/login',
     name: 'LoginPage',
     component: LoginPage
-  }]
+  }, {
+    path: '/register',
+    name: 'RegisterPage',
+    component: RegisterPage
+  }
+  ]
 
 })
