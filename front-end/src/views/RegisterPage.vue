50250c5d12e502fabd89d48600d1760cbe694e36
leeshinyook <tlsdbr97@gmail.com>
2020-02-03 22:42:36 +0900

    Test : RegisterPage.js 테스트 통과

--- front-end/src/views/RegisterPage.vue
+++ front-end/src/views/RegisterPage.vue
@@ -6,11 +6,17 @@
           <img class="logo" src="/static/images/logo.png">
           <div class="tagline">Open source task management tool</div>
         </div>
-        <form>
-          <div class="form-group"></div>
-          <div class="form-group"></div>
-          <div class="form-group"></div>
-          <button type="submit" class="btn btn-primary btn-block"></button>
+        <form @submit.prevent="submitForm">
+          <div class="form-group">
+            <input type="text" class="form-control" id="username" v-model="form.username">
+          </div>
+          <div class="form-group">
+            <input type="email" class="form-control" id="emailAddress" v-model="form.emailAddress">
+          </div>
+          <div class="form-group">
+            <input type="password" class="form-control" id="password" v-model="form.password">
+          </div>
+          <button type="submit" class="btn btn-primary btn-block">Create account</button>
           <p class="accept-terms text-muted">By clicking “Create account”, you agree to our <a href="#">terms of service</a> and <a href="#">privacy policy</a>.</p>
           <p class="text-center text-muted">Already have an account? <a href="/login">Sign in</a></p>
         </form>
@@ -30,7 +36,20 @@
 
 <script>
 export default {
-
+  name: 'RegisterPage',
+  data: function() {
+    return {
+      form: {
+        username: '',
+        emailAddress: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    submitForm() {
+    }
+  }
 }
 </script>
 
--- front-end/tests/unit/RegisterPage.spec.js
+++ front-end/tests/unit/RegisterPage.spec.js
@@ -1,17 +1,57 @@
 import Vue from 'vue'
 import RegisterPage from '@/views/RegisterPage'
+import { mount } from '@vue/test-utils'
 
 describe('RegisterPage.vue', () => {
+  let wrapper
+  let fieldUsername
+  let fieldEmailAddress
+  let filedPassword
+  let buttonSubmit
+
+  beforeEach(() => {
+    wrapper = mount(RegisterPage)
+    fieldUsername = wrapper.find('#username')
+    fieldEmailAddress = wrapper.find('#emailAddress')
+    filedPassword = wrapper.find('#password')
+    buttonSubmit = wrapper.find('form button[type=submit]')
+  })
+
+  // 렌더링 테스트
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(RegisterPage)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.logo').getAttribute('src'))
+    expect(wrapper.find('.logo').attributes().src)
       .toEqual('/static/images/logo.png')
-    expect(vm.$el.querySelector('.tagline').textContent)
+    expect(wrapper.find('.tagline').text())
       .toEqual('Open source task management tool')
-    expect(vm.$el.querySelector('#username').value).toEqual('')
-    expect(vm.$el.querySelector('#emailAddress').value).toEqual('')
-    expect(vm.$el.querySelector('#password').value).toEqual('')
-    expect(vm.$el.querySelector('formbutton[type="submit"]').textContent).toEqual('Create account')
+    expect(fieldUsername.element.value).toEqual('')
+    expect(fieldEmailAddress.element.value).toEqual('')
+    expect(filedPassword.element.value).toEqual('')
+    expect(buttonSubmit.text()).toEqual('Create account')
+  })
+  // 데이터모델 초기값 테스트
+  it('should contain data model with initial values', () => {
+    expect(wrapper.vm.form.username).toEqual('')
+    expect(wrapper.vm.form.emailAddress).toEqual('')
+    expect(wrapper.vm.form.password).toEqual('')
+  })
+  // 폼의 입력과 데이터 모델의 바인딩을 검증하는 테스트
+  it('should have form inputs bound with data model', () => {
+    const username = 'sunny'
+    const emailAddress = 'sunny@local'
+    const password = 'VueJsRocks'
+
+    wrapper.vm.form.username = username
+    wrapper.vm.form.emailAddress = emailAddress
+    wrapper.vm.form.password = password
+    expect(fieldUsername.element.value).toEqual(username)
+    expect(fieldEmailAddress.element.value).toEqual(emailAddress)
+    expect(filedPassword.element.value).toEqual(password)
+  })
+  // 제출 핸들러 존재 여부 확인하는 테스트
+  it('should have form submit event handler `submitForm`', () => {
+    const stub = jest.fn()
+    wrapper.setMethods({ submitForm: stub })
+    buttonSubmit.trigger('submit')
+    expect(stub).toBeCalled()
   })
 })
